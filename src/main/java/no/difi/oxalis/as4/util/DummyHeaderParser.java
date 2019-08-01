49c0b182240d090d13356d641504fadfc7ac9551
Even Østvold <Even.Ostvold@difi.no>
2019-08-01 11:17:32 +0100

    Added a DummyHeaderParser to facilitate CEF connectivity test.
    The parser simply returns an empty "no.difi.oxalis.api.header.Header"
    object. This is NOT recommended for production use. If you intend to
    use Oxalis outside of the PEPPOL context is is HIGHLY recommended that
    you familiarize yourself with the extension points of Oxalis and
    implements the affected interfaces.
    https://github.com/difi/oxalis/blob/master/doc/extension-points.adoc

---
+++ src/main/java/no/difi/oxalis/as4/util/DummyHeaderParser.java
@@ -0,0 +1,31 @@
+package no.difi.oxalis.as4.util;
+
+import com.google.inject.Singleton;
+import lombok.extern.slf4j.Slf4j;
+import no.difi.oxalis.api.header.HeaderParser;
+import no.difi.oxalis.api.util.Type;
+import no.difi.vefa.peppol.common.model.Header;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+@Slf4j
+@Singleton
+@Type("dummy")
+public class DummyHeaderParser implements HeaderParser {
+
+    @Override
+    public Header parse(InputStream inputStream) {
+
+        log.debug("DummyHeaderParser: parse");
+
+        try {
+            byte[] drain = new byte[500];
+            inputStream.read(drain);
+        } catch (IOException e) {
+            log.error("IOException while parsing header", e);
+        }
+
+        return new Header();
+    }
+}
